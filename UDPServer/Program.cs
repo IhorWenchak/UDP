8ffd10ac5abf62e070c659dc6c76eb20374921ee
Iven <iwenchak@gmail.com>
2020-07-17 21:47:28 +0300

    first commit

---
+++ UDPServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			const string ip = "176.120.56.169";
+			const int port = 8081;
+
+			var udpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+			var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+			udpSocket.Bind(udpEndPoint);
+
+			Random rnd = new Random();
+
+			while (true)
+			{				
+
+				int value = rnd.Next();
+
+				var serverEndPoint = new IPEndPoint(IPAddress.Parse(ip), 8082); 
+
+				udpSocket.SendTo(Encoding.UTF8.GetBytes(value.ToString()), serverEndPoint);
+
+			}
+
+		}
+	}
+}
---
+++ UPDClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string ip = "176.120.56.169";
+			const int port = 8082;
+
+			var udpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+			var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+			udpSocket.Bind(udpEndPoint);
+
+			while(true)
+			{
+				var buffer = new byte[256];
+				var size = 0;
+				var data = new StringBuilder();
+				EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+				do
+				{
+					size = udpSocket.ReceiveFrom(buffer, ref senderEndPoint);
+					data.Append(Encoding.UTF8.GetString(buffer));
+					data.Append("\n");
+				}
+				while (udpSocket.Available > 0);
+
+				Console.WriteLine(data);
+
+			}
+
+		}
+	}
+}
